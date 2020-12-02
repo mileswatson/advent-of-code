a5211d53890ba76a60356e77f41cea250d4bbaa5
Miles Watson <32907412+mileswatson@users.noreply.github.com>
2020-12-02 15:33:04 +0000

    Added solution to challenge 2.

---
+++ 2 - Password Philosophy/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+type Line struct {
+    password    string
+    target      string
+    num1     int
+    num2     int
+}
+
+func main() {
+
+    data, _ := ioutil.ReadFile("input.txt")
+
+    lineArray := strings.Split(string(data), "\n")
+
+    lineSlice := lineArray[:]
+
+    var lines = make([]*Line, len(lineSlice))
+
+    for i := 0; i < len(lineSlice); i++ {
+        lines[i] = new(Line)
+        fmt.Sscanf(lineSlice[i], "%d-%d %s %s",
+            &lines[i].num1,
+            &lines[i].num2,
+            &lines[i].target,
+            &lines[i].password,
+        )
+        lines[i].target = lines[i].target[:1]
+    }
+    
+    fmt.Println(partOne(lines))
+    fmt.Println(partTwo(lines))
+
+}
+
+func partOne(lines []*Line) int {
+    total := 0
+
+    for _, line := range lines {
+        num := strings.Count(line.password, line.target)
+        if (line.num1 <= num && num <= line.num2) {
+            total++
+        }
+    }
+
+    return total
+}
+
+func partTwo(lines []*Line) int {
+    total := 0
+
+    for _, line := range lines {
+        atOne := line.password[line.num1-1:line.num1] == line.target
+        atTwo := line.password[line.num2-1:line.num2] == line.target
+
+        if(atOne || atTwo) && !(atOne && atTwo) {
+            total++
+        }
+            
+    }
+
+    return total
+}
