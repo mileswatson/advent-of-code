088bbb9db260d645ef57b2bd92630c682d523393
Miles Watson <milestwatson@icloud.com>
2020-12-05 14:14:32 +0000

    Fixed some errors on Windows.

--- 2020/2 - Password Philosophy/main.go
+++ 2020/2 - Password Philosophy/main.go
@@ -1,69 +1,72 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
+// Line is a struct to represent each password and policy
 type Line struct {
-    password    string
-    target      string
-    num1     int
-    num2     int
+	password string
+	target   string
+	num1     int
+	num2     int
 }
 
 func main() {
 
-    data, _ := ioutil.ReadFile("input.txt")
+	data, _ := ioutil.ReadFile("input.txt")
 
-    lineArray := strings.Split(string(data), "\n")
+	str := strings.ReplaceAll(string(data), "\r", "")
 
-    lineSlice := lineArray[:]
+	lineArray := strings.Split(str, "\n")
 
-    var lines = make([]*Line, len(lineSlice))
+	lineSlice := lineArray[:]
 
-    for i := 0; i < len(lineSlice); i++ {
-        lines[i] = new(Line)
-        fmt.Sscanf(lineSlice[i], "%d-%d %s %s",
-            &lines[i].num1,
-            &lines[i].num2,
-            &lines[i].target,
-            &lines[i].password,
-        )
-        lines[i].target = lines[i].target[:1]
-    }
-    
-    fmt.Println(partOne(lines))
-    fmt.Println(partTwo(lines))
+	var lines = make([]*Line, len(lineSlice))
+
+	for i := 0; i < len(lineSlice); i++ {
+		lines[i] = new(Line)
+		fmt.Sscanf(lineSlice[i], "%d-%d %s %s",
+			&lines[i].num1,
+			&lines[i].num2,
+			&lines[i].target,
+			&lines[i].password,
+		)
+		lines[i].target = lines[i].target[:1]
+	}
+
+	fmt.Println(partOne(lines))
+	fmt.Println(partTwo(lines))
 
 }
 
 func partOne(lines []*Line) int {
-    total := 0
+	total := 0
 
-    for _, line := range lines {
-        num := strings.Count(line.password, line.target)
-        if (line.num1 <= num && num <= line.num2) {
-            total++
-        }
-    }
+	for _, line := range lines {
+		num := strings.Count(line.password, line.target)
+		if line.num1 <= num && num <= line.num2 {
+			total++
+		}
+	}
 
-    return total
+	return total
 }
 
 func partTwo(lines []*Line) int {
-    total := 0
+	total := 0
 
-    for _, line := range lines {
-        atOne := line.password[line.num1-1:line.num1] == line.target
-        atTwo := line.password[line.num2-1:line.num2] == line.target
+	for _, line := range lines {
+		atOne := line.password[line.num1-1:line.num1] == line.target
+		atTwo := line.password[line.num2-1:line.num2] == line.target
 
-        if(atOne || atTwo) && !(atOne && atTwo) {
-            total++
-        }
-            
-    }
+		if (atOne || atTwo) && !(atOne && atTwo) {
+			total++
+		}
 
-    return total
+	}
+
+	return total
 }
--- 2020/4 - Passport Processing/main.go
+++ 2020/4 - Passport Processing/main.go
@@ -1,174 +1,176 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "strings"
-    "strconv"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-    data, _ := ioutil.ReadFile("input.txt")
+	data, _ := ioutil.ReadFile("input.txt")
 
-    lineArray := strings.Split(string(data), "\n")
+	str := strings.ReplaceAll(string(data), "\r", "")
 
-    lines := lineArray[:]
-    
-    fmt.Println(partOne(lines))
-    fmt.Println(partTwo(lines))
+	lineArray := strings.Split(str, "\n")
+
+	lines := lineArray[:]
+
+	fmt.Println(partOne(lines))
+	fmt.Println(partTwo(lines))
 
 }
 
 func partOne(lines []string) int {
-    passports := combine(lines)
-    total := 0
-    for _, passport := range passports {
-        if containsAllFields(passport) {
-            total++
-        }
-    }
-    return total;
+	passports := combine(lines)
+	total := 0
+	for _, passport := range passports {
+		if containsAllFields(passport) {
+			total++
+		}
+	}
+	return total
 }
 
 func partTwo(lines []string) int {
-    passports := combine(lines)
-    total := 0
-    for _, passport := range passports {
-        if (!containsAllFields(passport)) {
-            continue
-        }
-        var valid bool
-        for _, field := range getFields(passport) {
-            switch field.name {
-            case "byr":
-                valid = digits(field.value, 4, 1920, 2002)
-            case "iyr":
-                valid = digits(field.value, 4, 2010, 2020)
-            case "eyr":
-                valid = digits(field.value, 4, 2020, 2030)
-            case "hgt":
-                length := len(field.value)
-                units := field.value[length-2:length]
-                value := field.value[:length-2]
-                if (units == "cm") {
-                    valid = digits(value, 3, 150, 193)
-                } else if (units == "in") {
-                    valid = digits(value, 2, 59, 76)
-                } else {
-                    valid = false
-                }
-            case "hcl":
-                if (field.value[0:1] != "#") {
-                    valid = false
-                }
-                valid = allChars(field.value[1:], "0123456789abcdef", 6)
-            case "ecl":
-                combinations := []string {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-                valid = contains(combinations, field.value)
-            case "pid":
-                valid = allChars(field.value, "0123456789", 9)
-            case "cid":
-                valid = true
-            default:
-                valid = false
-            }
-            if !valid {
-                break
-            }
-        }
-        if valid {
-            total++
-        }
-    }
+	passports := combine(lines)
+	total := 0
+	for _, passport := range passports {
+		if !containsAllFields(passport) {
+			continue
+		}
+		var valid bool
+		for _, field := range getFields(passport) {
+			switch field.name {
+			case "byr":
+				valid = digits(field.value, 4, 1920, 2002)
+			case "iyr":
+				valid = digits(field.value, 4, 2010, 2020)
+			case "eyr":
+				valid = digits(field.value, 4, 2020, 2030)
+			case "hgt":
+				length := len(field.value)
+				units := field.value[length-2 : length]
+				value := field.value[:length-2]
+				if units == "cm" {
+					valid = digits(value, 3, 150, 193)
+				} else if units == "in" {
+					valid = digits(value, 2, 59, 76)
+				} else {
+					valid = false
+				}
+			case "hcl":
+				if field.value[0:1] != "#" {
+					valid = false
+				}
+				valid = allChars(field.value[1:], "0123456789abcdef", 6)
+			case "ecl":
+				combinations := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+				valid = contains(combinations, field.value)
+			case "pid":
+				valid = allChars(field.value, "0123456789", 9)
+			case "cid":
+				valid = true
+			default:
+				valid = false
+			}
+			if !valid {
+				break
+			}
+		}
+		if valid {
+			total++
+		}
+	}
 
-    return total;
+	return total
 }
 
 func contains(s []string, e string) bool {
-    for _, a := range s {
-        if a == e {
-            return true
-        }
-    }
-    return false
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func allChars(str, allowed string, length int) bool {
-    if len(str) != length {
-        return false
-    }
-    valid := true
-    for _, char := range str {
-        if !strings.Contains(str, string(char)) {
-            valid = false
-            break
-        }
-    }
-    return valid
+	if len(str) != length {
+		return false
+	}
+	valid := true
+	for _, char := range str {
+		if !strings.Contains(str, string(char)) {
+			valid = false
+			break
+		}
+	}
+	return valid
 }
 
 func combine(lines []string) []string {
-    var passports []string
-    combined := ""
-    for _, line := range lines {
-        if (len(line) != 0) {
-            combined += line + " "
-            continue
-        }
-        passports = append(passports, combined[:len(combined)-1])
-        combined = ""
-    }
-    return passports
+	var passports []string
+	combined := ""
+	for _, line := range lines {
+		if len(line) != 0 {
+			combined += line + " "
+			continue
+		}
+		passports = append(passports, combined[:len(combined)-1])
+		combined = ""
+	}
+	return passports
 }
 
 func containsAllFields(passport string) bool {
-    required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
-    allFound := true
-    for _, field := range required {
-        if !strings.Contains(passport, field) {
-            allFound = false
-            break
-        }
-    }
+	allFound := true
+	for _, field := range required {
+		if !strings.Contains(passport, field) {
+			allFound = false
+			break
+		}
+	}
 
-    return allFound;
+	return allFound
 }
 
+// Field is a struct to represent a passport field
 type Field struct {
-    name    string
-    value   string
+	name  string
+	value string
 }
 
 func getFields(passport string) []Field {
-    var fields []Field
-    parts := strings.Split(passport, " ")
+	var fields []Field
+	parts := strings.Split(passport, " ")
+	for _, part := range parts {
+		field := Field{
+			part[:3], part[4:],
+		}
+		fields = append(fields, field)
+	}
 
-    for _, part := range parts {
-        field := Field{
-            part[:3], part[4:],
-        }
-        fields = append(fields, field)
-    }
-
-    return fields
+	return fields
 }
 
 func digits(str string, num, min, max int) bool {
-    if (len(str) != num) {
-        return false
-    }
+	if len(str) != num {
+		return false
+	}
 
-    i, err := strconv.Atoi(str)
+	i, err := strconv.Atoi(str)
 
-    if err != nil {
-        return false;
-    }
+	if err != nil {
+		return false
+	}
 
-    if i < min || i > max {
-        return false;
-    }
+	if i < min || i > max {
+		return false
+	}
 
-    return true;
+	return true
 }
